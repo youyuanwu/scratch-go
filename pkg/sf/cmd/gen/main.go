3b205b6ead8844af9008357e8ecd94a91bdebffb
Youyuan Wu <youyuanwu@outlook.com>
2023-09-23 16:51:08 -0700

    save work to generate sf go code

---
+++ pkg/sf/cmd/gen/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/zzl/go-winapi-gen/codegen"
+	"github.com/zzl/go-winapi-gen/gomodel"
+	"github.com/zzl/go-winapi-gen/utils"
+	"github.com/zzl/go-winmd/apimodel"
+	"github.com/zzl/go-winmd/mdmodel"
+)
+
+func main() {
+
+	mdFilePath := ".windows/winmd/Microsoft.ServiceFabric.winmd"
+	outputDir := "output"
+
+	os.MkdirAll(outputDir, os.ModePerm)
+	utils.CleanDir(outputDir)
+
+	mdModelParser := mdmodel.NewModelParser()
+	mdModel, err := mdModelParser.Parse(mdFilePath)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer mdModel.Close()
+
+	apiModelParser := apimodel.NewModelParser(map[string]*apimodel.Type{
+		"System.Guid": {
+			Name:     "GUID",
+			FullName: "syscall.GUID",
+			Kind:     apimodel.TypeStruct,
+			Struct:   true,
+			SiezInfo: &apimodel.SizeInfo{16, 4},
+		},
+	})
+	apiModel := apiModelParser.Parse(mdModel)
+
+	//apiFilter := &gomodel.ApiFilter{
+	//Namespaces: []string{
+	//	"Windows.Foundation*",
+	//	"Windows.Networking",
+	//	"Windows.Devices.Bluetooth*",
+	//	"Windows.Devices.Enumeration",
+	//	"Windows.Web.Http*",
+	//	"Windows.Storage.Streams",
+	//	"!Windows.Foundation.Diagnostics",
+	//	"!Windows.Web.Http.Diagnostics",
+	//},
+	//}
+
+	// filter desktop APIs
+	apiFilter := &gomodel.ApiFilter{}
+	modelParser := gomodel.NewModelParser(apiModel, nil, nil)
+	goModel := modelParser.Parse()
+	for _, pkg := range goModel.Packages {
+		if len(pkg.RtClasses) > 0 {
+			apiFilter.Namespaces = append(apiFilter.Namespaces, pkg.FullName)
+		}
+	}
+	//add additional dependent nss
+	// apiFilter.Namespaces = append(apiFilter.Namespaces, "Windows.UI.Popups")
+	// apiFilter.Namespaces = append(apiFilter.Namespaces, "Windows.Foundation.Numerics")
+	// //remove unwanted nss
+	// apiFilter.Namespaces = append(apiFilter.Namespaces, "!Windows.Management.Deployment*")
+	// apiFilter.Namespaces = append(apiFilter.Namespaces, "!Windows.Graphics.Holographic")
+
+	//
+	modelParser = gomodel.NewModelParser(apiModel, apiFilter, map[string]*gomodel.Type{
+		"System.Guid": gomodel.TypeGuid,
+	})
+	goModel = modelParser.Parse()
+
+	generator := codegen.NewGenerator(goModel, map[string]string{
+		//"Windows.*": "winrt",
+	})
+	generator.OutputDir = outputDir
+	generator.PackageRootPath = "github.com/youyuanwu/scratch-go/pkg/sf/output"
+	generator.NsFullNameAsFileName = true
+	generator.FileNamePrefixToStrip = "Windows."
+	generator.PrefixEnumValuesWithTypeName = true
+	generator.Gen()
+
+	absOutput, _ := filepath.Abs(outputDir)
+	_ = exec.Command("gofmt", "-s", "-w", absOutput).Run()
+	println("Done.")
+}
---
+++ pkg/sf/go.mod
@@ -0,0 +1,13 @@
+module github.com/youyuanwu/scratch-go/pkg/sf
+
+go 1.19
+
+require (
+	github.com/zzl/go-winapi-gen v1.0.0
+	github.com/zzl/go-winmd v1.0.0
+)
+
+require (
+	github.com/zzl/go-win32api v1.1.3 // indirect
+	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
+)
